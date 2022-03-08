7a22661528cecd667f0e0b12084a653c23048c91
Griffin Lamphere <7319002@isd535.org>
2022-03-08 11:30:19 -0600

    added "add" function to datastore

---
+++ scripts/datastore.js
@@ -0,0 +1,18 @@
+(function (window) {
+    'use strict'
+    
+    let App = window.App || {};
+
+    function DataStore() {
+        console.log('Running the DataStore function...');
+        this.data = {};
+    }
+
+    DataStore.prototype.add = function (key, val) {
+        this.data[key] = val;
+    };
+
+    App.DataStore = DataStore;
+    window.App = App;
+
+})(window);
