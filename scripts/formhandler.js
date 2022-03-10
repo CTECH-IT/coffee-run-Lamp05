f5da7a9809b49b7f8424ecbea3d08d609e000a93
Griffin Lamphere <7319002@isd535.org>
2022-03-10 11:24:15 -0600

    added FormHandler empty function

---
+++ scripts/formhandler.js
@@ -0,0 +1,13 @@
+(function (window) {
+    'use strict'
+
+    let App = window.App || {};
+
+    function FormHandler() {
+
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+
+})(window);
