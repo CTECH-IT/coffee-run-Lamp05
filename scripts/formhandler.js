fcf447b1f4d5cc2d7cfb2993e17cfac550982979
Griffin Lamphere <7319002@isd535.org>
2022-03-14 11:18:19 -0500

    can now submit orders

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -4,16 +4,36 @@
     let App = window.App || {};
     let $ = window.jQuery;
 
-    function FormHandler() {
+    function FormHandler(selector) {
         if (!selector) {
             throw new Error('No selector provided!');
         }
+
+
+        // find the "selector" in the DOM using jQuery and assign it to this.formElement
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
     }
 
-    // find the "selector" in the DOM using jQuery and assign it to this.formElement
-    this.$formElement = $(selector);
-    if (this.$formElement.length == 0) {
-        throw new Error('Could not find element with selector: ' + selector);
+    // Add an event handler for the Submit button and pass in createOrder as a parameter (func)
+    FormHandler.prototype.addSubmitHandler = function (func) {
+        console.log('Setting the submit handler for the form...');
+        this.$formElement.on('submit', function (event) {
+            event.preventDefault();
+
+            // copy all the info from the form fields into the variable called data
+            let data = {};
+            $(this).serializeArray().forEach(function (item) {
+                data[item.name] = item.value;
+                console.log(item.name + ' is ' + item.value);
+            });
+            console.log(data);
+            func(data); // call the function that was passed in on the data from the form
+
+            this.reset(); // reset the form
+        });
     }
 
     App.FormHandler = FormHandler;
