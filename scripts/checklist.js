e5830b4f2d454e4e326434097312ebdafd5a1ab4
Griffin Lamphere <7319002@isd535.org>
2022-03-16 10:33:42 -0500

    addrow in checklist js

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -12,27 +12,42 @@
         }
     }
 
+    // the method that adds a new row to the checklist
+    CheckList.prototype.addRow = function (coffeeOrder) {
+        // Create a new instance of a row, using the coffee order info
+        var rowElement = new Row(coffeeOrder);
+        // Add the new row instance's $element propterty to the checklist
+        this.$element.append(rowElement.$element);
+    };
+
     // Each row is one outstanding order
     function Row(coffeeOrder) {
         let $div = $('<div></div>', {
             'data-coffee-order': 'checkbox',
             'class': 'checkbox'
-            });
+        });
         let $label = $('<label></label>');
 
         let $checkbox = $('<input></input>', {
             type: 'checkbox',
             value: coffeeOrder.emailAddress
-            });
+        });
     }
 
     let description = coffeeOrder.size + ' ';
     if (coffeeOrder.flavor) {
         description += coffeeOrder.flavor + ' ';
+
+        description += coffeeOrder.coffee + ', ';
+        description += ' (' + coffeeOrder.emailAddress + ')';
+        description += ' [' + coffeeOrder.strength + 'x]';
+
+        $label.append($checkbox);
+        $label.append(description);
+        $div.append($label);
+
+        this.$element = $div;
     }
-    description += coffeeOrder.coffee + ', ';
-    description += ' (' + coffeeOrder.emailAddress + ')';
-    description += ' [' + coffeeOrder.strength + 'x]';
 
     // Add the Checklist to the App namespace
     App.Checklist = CheckList;
