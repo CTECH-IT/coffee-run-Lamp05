dc91146cdf76d167f34019c646a14095889f39bf
Griffin Lamphere <7319002@isd535.org>
2022-03-16 10:43:14 -0500

    changed formhandler

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,25 @@
     'use strict';
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
 
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList;
 
     let myTruck = new Truck('12345', new DataStore());
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
 
     window.myTruck = myTruck;
 
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
+    
+    // when the submit button is called, create the order and add a checkbox
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
 
 })(window);
